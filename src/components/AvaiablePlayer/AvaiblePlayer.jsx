d824261a969569b5be1790bb4e8b52a067f28099
delowarshizar <husain22205101453@diu.edu.bd>
2026-01-20 02:20:31 +0600

    player card ready fo responsive

--- src/components/AvaiablePlayer/AvaiblePlayer.jsx
+++ src/components/AvaiablePlayer/AvaiblePlayer.jsx
@@ -5,7 +5,7 @@ const AvaiblePlayer = ({ PlayerData }) => {
   const Players = use(PlayerData);
 
   return (
-    <div className="max-w-[1200px] mx-auto grid grid-cols-3 gap-6">
+    <div className="max-w-[1200px] mx-auto w-full grid md:grid-cols-2 lg:grid-cols-3 gap-6">
       {Players.map((player) => (
         <div className="card bg-base-100 shadow-sm p-4 mt-5">
           <figure>
