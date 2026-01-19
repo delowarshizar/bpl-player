45bd5a080693c3d57ad816d362834caceb42b195
delowarshizar <husain22205101453@diu.edu.bd>
2026-01-19 23:31:05 +0600

    file updated

--- src/components/AvaiablePlayer/AvaiblePlayer.jsx
+++ src/components/AvaiablePlayer/AvaiblePlayer.jsx
@@ -2,20 +2,12 @@ import React, { use } from "react";
 
 const AvaiblePlayer = ({ PlayerData }) => {
   const Players = use(PlayerData);
+  console.log(Players)
 
   return (
     <div>
       <h1>Available Player</h1>
       <div>
-        {Players.map((player) => (
-          <div>
-            <img src={player.player_img} alt={player.name} className="w-32 " />
-            <h2 className="text-xl font-bold mt-2">{player.name}</h2>
-            <p>Country: {player.country}</p>
-            <p>Rating: {player.rating}</p>
-            
-          </div>
-        ))}
       </div>
     </div>
   );
