45a692a20b54137021374eadb59f17a31e6f2c7d
delowarshizar <husain22205101453@diu.edu.bd>
2026-01-21 11:00:13 +0600

    toggle selection result and added all images

--- src/components/AvaiablePlayer/AvaiblePlayer.jsx
+++ src/components/AvaiablePlayer/AvaiblePlayer.jsx
@@ -1,44 +1,12 @@
 import React, { use } from "react";
-import PlayerIcon from "../../assets/user1.png";
-import FlagIcon from "../../assets/report1.png";
+import PlayerCard from "../PlayerCard/PlayerCard";
 const AvaiblePlayer = ({ PlayerData }) => {
   const Players = use(PlayerData);
 
   return (
     <div className="max-w-[1200px] mx-auto w-full grid md:grid-cols-2 lg:grid-cols-3 gap-6">
       {Players.map((player) => (
-        <div className="card bg-base-100 shadow-sm p-4 mt-5">
-          <figure>
-            <img
-              className="rounded-xl h-[200px] object-contain"
-              src={player.player_img}
-              alt="Shoes"
-            />
-          </figure>
-          <div className="flex gap-4 mt-4 items-center px-2">
-            <img src={PlayerIcon} alt="" />
-            <h2 className="card-title">{player.name}</h2>
-          </div>
-          <div className="mt-4 flex justify-between p-2 border-b-gray-300 border-b">
-            <div className="flex gap-4 items-center">
-              <img src={FlagIcon} alt="" />
-              <p>{player.country}</p>
-            </div>
-            <button className="btn"> {player.player_role}</button>
-          </div>
-          <div className="flex justify-between px-2 mt-4">
-            <span className="font-bold">Rating</span>
-            <span>{player.rating}</span>
-          </div>
-          <div className="mt-4 flex px-2 items-center justify-between">
-            <span className="font-bold"> {player.batting_style}</span>
-            <span> {player.bowling_style}</span>
-          </div>
-          <div className="card-actions mt-4 flex px-2 items-center justify-between ">
-            <span className="font-bold">Price: {player.price}</span>
-            <button className="btn ">Choose Player</button>
-          </div>
-        </div>
+        <PlayerCard player={player}></PlayerCard>
       ))}
     </div>
   );
